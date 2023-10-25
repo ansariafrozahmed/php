fda5c4e8b0ba9acf2bd375513915c6f970bb94df
Ansari Afroz Ahmed <137764964+ansariafrozahmed@users.noreply.github.com>
2023-10-25 17:13:38 +0530

    Delete PHP directory

--- PHP/database/db_connect.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-$connect = mysqli_connect('localhost', 'root' , '', 'st_users');
-if (!$connect) {
-    echo 'Database Connection Error';
-}
-
-?>
--- PHP/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy();
-// echo '<script>alert("Successfully Log Out!!")</script>';
-header('Location: login.php');
-?>
